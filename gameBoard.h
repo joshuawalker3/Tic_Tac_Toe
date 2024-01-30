01c505c46f026f610de8342f716576dc56c9faad
Josh <walkerje1992@gmail.com>
2024-01-29 21:20:09 -0700

    Initial upload. Not really started

---
+++ gameBoard.h
@@ -0,0 +1,23 @@
+//
+// Created by joshua on 1/29/24.
+//
+
+#ifndef TIC_TAC_TOE_GAMEBOARD_H
+#define TIC_TAC_TOE_GAMEBOARD_H
+
+#include <stdbool.h>
+
+struct boardSpace {
+    bool filled;
+    int val;
+};
+
+void printBlankBoard() {
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; i < 5; i++) {
+            
+        }
+    }
+}
+
+#endif //TIC_TAC_TOE_GAMEBOARD_H
